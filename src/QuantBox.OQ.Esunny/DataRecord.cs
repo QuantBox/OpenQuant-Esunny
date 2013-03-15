f249ecccea5643260052073d98636f2401afecd8
伍侃 <wu-kan@163.com>
2013-03-15 14:22:59 +0800

    Tick数据支持时间段

---
+++ src/QuantBox.OQ.Esunny/DataRecord.cs
@@ -0,0 +1,19 @@
+﻿using SmartQuant.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuantBox.OQ.Esunny
+{
+    class DataRecord
+    {
+        public string key;
+        public string market;
+        public string symbol;
+        public HistoricalDataRequest request;
+
+        // 请求的当前时间
+        public DateTime date;
+    }
+}
--- src/QuantBox.OQ.Esunny/EsunnyProvider.HistoricalDataProvider.cs
+++ src/QuantBox.OQ.Esunny/EsunnyProvider.HistoricalDataProvider.cs
@@ -34,11 +34,8 @@ namespace QuantBox.OQ.Esunny
         [Category(CATEGORY_HISTORICAL)]
         public int MaxConcurrentRequests
         {
-            get { return -1; }
+            get { return 3; }
         }
-
-        private Dictionary<string, HistoricalDataRequest> historicalDataIds = new Dictionary<string, HistoricalDataRequest>();
-        private Dictionary<string, string> historicalDataRecords = new Dictionary<string, string>();
 
         public event HistoricalDataEventHandler HistoricalDataRequestCancelled;
 
@@ -106,13 +103,20 @@ namespace QuantBox.OQ.Esunny
         }
 
         #region IHistoricalDataProvider
+        // 通过key能找到
+        private Dictionary<string, DataRecord> historicalDataRecords_key = new Dictionary<string, DataRecord>();
+        // 通过请求ID也能找到
+        private Dictionary<string, DataRecord> historicalDataRecords_requestId = new Dictionary<string, DataRecord>();
+
         public void CancelHistoricalDataRequest(string requestId)
         {
-            if (historicalDataIds.ContainsKey(requestId))
+            if (historicalDataRecords_requestId.ContainsKey(requestId))
             {
-                HistoricalDataRequest request = historicalDataIds[requestId] as HistoricalDataRequest;
-                historicalDataIds.Remove(requestId);
-                EmitHistoricalDataCancelled(request);
+                DataRecord dr = historicalDataRecords_requestId[requestId] as DataRecord;
+                EmitHistoricalDataCancelled(dr.request);
+
+                historicalDataRecords_requestId.Remove(dr.request.RequestId);
+                historicalDataRecords_key.Remove(dr.key);
             }
         }
 
@@ -147,6 +151,86 @@ namespace QuantBox.OQ.Esunny
         }
         #endregion
 
+        #region 数据结构维护
+        private void SaveRequest(HistoricalDataRequest request, string key, string market, string symbol, DateTime date)
+        {
+            DataRecord dr;
+            if (!historicalDataRecords_key.TryGetValue(key, out dr))
+            {
+                dr = new DataRecord();
+                dr.key = key;
+                dr.market = market;
+                dr.symbol = symbol;
+                dr.request = request;
+            }
+            historicalDataRecords_key[key] = dr;
+            historicalDataRecords_requestId[request.RequestId] = dr;
+
+            dr.date = date;
+        }
+
+        private void RemoveRequest(string key)
+        {
+            DataRecord dr;
+            if (!historicalDataRecords_key.TryGetValue(key, out dr))
+            {
+                return;
+            }
+            if (dr.date >= dr.request.EndDate)
+            {
+                EmitHistoricalDataCompleted(dr.request);
+
+                historicalDataRecords_requestId.Remove(dr.request.RequestId);
+                historicalDataRecords_key.Remove(dr.key);
+                return;
+            }
+        }
+
+        private void SendRequest_Tick(string key)
+        {
+            DataRecord dr;
+            if (!historicalDataRecords_key.TryGetValue(key, out dr))
+            {
+                return;
+            }
+            
+            // 移动到下一交易日
+            do
+            {
+                dr.date = dr.date.AddDays(1);
+                if(dr.date.DayOfWeek == DayOfWeek.Saturday
+                    ||dr.date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }while(true);
+            
+            // 超出日期了,可以中止了
+            if (dr.date >= dr.request.EndDate)
+            {
+                EmitHistoricalDataCompleted(dr.request);
+
+                historicalDataRecords_requestId.Remove(dr.request.RequestId);
+                historicalDataRecords_key.Remove(dr.key);
+                return;
+            }
+
+            // 发送请求
+            int nRet = QuotApi.QT_RequestTrace(m_pQuotApi, dr.market, dr.symbol, dr.date.ToString("yyyyMMdd"));
+            if (0 == nRet)
+            {
+            }
+            else
+            {
+                EmitHistoricalDataError(dr.request, "API返回错误:" + nRet);
+            }
+        }
+        #endregion
+
         #region Tick数据
         string GetKeyFromSTKTRACEDATA(ref STKTRACEDATA pTraceData)
         {
@@ -158,97 +242,94 @@ namespace QuantBox.OQ.Esunny
             return string.Format("{0}:{1}", market, altSymbol);
         }
 
+        // 同一合约的一次只能查一天，如果同时查多天就没法区分哪天返回的为0
         public void SendHistoricalDataRequestTick(HistoricalDataRequest request, string market)
         {
+            TimeSpan ts = request.EndDate - request.BeginDate;
+            if (ts.Days <= 0)
+            {
+                EmitHistoricalDataError(request, "开始与结束为同一天");
+                return;
+            }
+            else if(ts.Days>7)
+            {
+                EmitHistoricalDataError(request, "为减少服务器负担，Tick数据一次只能取一周");
+                return;
+            }
+            
             Instrument inst = request.Instrument as Instrument;
             string altSymbol = inst.GetSymbol(Name);
 
-            string beginDate = request.BeginDate.ToString("yyyyMMdd");
-            string endDate = request.EndDate.ToString("yyyyMMdd");
-            string date = beginDate;
-
             string key = GetKeyFromSTKTRACEDATA(market, altSymbol);
 
-            historicalDataIds[request.RequestId] = request;
-            historicalDataRecords[key] = request.RequestId;
-
-            int nRet = QuotApi.QT_RequestTrace(m_pQuotApi, market, altSymbol, date);
-            if (0 == nRet)
-            {
-                historicalDataIds[request.RequestId] = request;
-                historicalDataRecords[key] = request.RequestId;
-            }
-            else
-            {
-                EmitHistoricalDataError(request, "API返回错误:" + nRet);
-            }
+            // 先存下来再发请求
+            SaveRequest(request, key, market, altSymbol, request.BeginDate.AddDays(-1));
+            SendRequest_Tick(key);
         }
 
         private void OnRspTraceData(IntPtr pQuotApi, IntPtr pBuffer, ref STKTRACEDATA pTraceData)
         {
             string key = GetKeyFromSTKTRACEDATA(ref pTraceData);
-            string requestId;
-            if (historicalDataRecords.TryGetValue(key, out requestId))
+            DataRecord dr;
+            if (historicalDataRecords_key.TryGetValue(key, out dr))
             {
-                HistoricalDataRequest request;
-                if (historicalDataIds.TryGetValue(requestId, out request))
+                HistoricalDataRequest request = dr.request;
+
+                int day = -1;
+                float volume = 0;
+                DateTime datetime = DateTime.Now;
+                DateTime updatetime = DateTime.Now;
+
+                IntPtr ptrHead = (IntPtr)(pBuffer + Marshal.SizeOf(typeof(STKTRACEDATA)));
+                for (int i = 0; i < pTraceData.nCount; ++i)
                 {
-                    int day = -1;
-                    float volume = 0;
-                    DateTime datetime = DateTime.Now;
-                    DateTime updatetime = DateTime.Now;
+                    IntPtr ptr = (IntPtr)(ptrHead + Marshal.SizeOf(typeof(STOCKTRACEDATA)) * i);
+                    STOCKTRACEDATA std = (STOCKTRACEDATA)Marshal.PtrToStructure(ptr, typeof(STOCKTRACEDATA));
 
-                    IntPtr ptrHead = (IntPtr)(pBuffer + Marshal.SizeOf(typeof(STKTRACEDATA)));
-                    for (int i = 0; i < pTraceData.nCount; ++i)
+                    DateTime dt = Convert.ToDateTime(std.time);
+                    if (datetime == dt)
                     {
-                        IntPtr ptr = (IntPtr)(ptrHead + Marshal.SizeOf(typeof(STOCKTRACEDATA)) * i);
-                        STOCKTRACEDATA std = (STOCKTRACEDATA)Marshal.PtrToStructure(ptr, typeof(STOCKTRACEDATA));
+                        updatetime = updatetime.AddMilliseconds(250);
+                    }
+                    else
+                    {
+                        updatetime = dt;
+                    }
+                    if (day != updatetime.Day)
+                    {
+                        volume = 0;
+                    }
+                    day = updatetime.Day;
+                    volume = std.m_Volume - volume;
 
-                        DateTime dt = Convert.ToDateTime(std.time);
-                        if (datetime == dt)
+                    if (updatetime >= request.BeginDate && updatetime < request.EndDate)
+                    {
+                        if (request.DataType == HistoricalDataType.Trade)
                         {
-                            updatetime = updatetime.AddMilliseconds(250);
+                            Trade trade = new Trade(updatetime, std.m_NewPrice, (int)volume);
+                            NewHistoricalTrade(this,
+                                new HistoricalTradeEventArgs(trade, request.RequestId, request.Instrument, this, -1));
                         }
                         else
                         {
-                            updatetime = dt;
+                            Quote quote = new Quote(updatetime, std.m_BuyPrice, (int)std.m_BuyVol, std.m_SellPrice, (int)std.m_SellVol);
+                            NewHistoricalQuote(this,
+                                new HistoricalQuoteEventArgs(quote, request.RequestId, request.Instrument, this, -1));
                         }
-                        if (day != updatetime.Day)
-                        {
-                            volume = 0;
-                        }
-                        day = updatetime.Day;
-                        volume = std.m_Volume - volume;
-                        
-                        if (updatetime >= request.BeginDate && updatetime < request.EndDate)
-                        {
-                            if (request.DataType == HistoricalDataType.Trade)
-                            {
-                                Trade trade = new Trade(updatetime, std.m_NewPrice, (int)volume);
-                                NewHistoricalTrade(this,
-                                    new HistoricalTradeEventArgs(trade, request.RequestId, request.Instrument, this, -1));
-                            }
-                            else
-                            {
-                                Quote quote = new Quote(updatetime, std.m_BuyPrice, (int)std.m_BuyVol, std.m_SellPrice, (int)std.m_SellVol);
-                                NewHistoricalQuote(this,
-                                    new HistoricalQuoteEventArgs(quote, request.RequestId, request.Instrument, this, -1));
-                            }
-                        }
-
-                        datetime = dt;
-                        volume = std.m_Volume;
                     }
 
-                    historicalDataIds.Remove(request.RequestId);
-                    EmitHistoricalDataCompleted(request);
+                    datetime = dt;
+                    volume = std.m_Volume;
                 }
-                historicalDataRecords.Remove(key);
+
+                RemoveRequest(key);
+                SendRequest_Tick(key);
             }
         }
         #endregion
 
         #region 历史Bar数据
+        // 将请求的周期传成易盛的周期类型
         PeriodType GetPeriodTypeFromDataType(HistoricalDataRequest request)
         {
             if (request.DataType == HistoricalDataType.Daily)
@@ -272,16 +353,19 @@ namespace QuantBox.OQ.Esunny
             return PeriodType.MAX_PERIOD_TYPE;
         }
 
+        // 生成历史数据的请求Key
         string GetKeyFromSTKHISDATA(ref STKHISDATA pHisData)
         {
             return string.Format("{0}:{1}:{2}", pHisData.Market, pHisData.Code, pHisData.nPeriod);
         }
 
+        // 生成历史数据的请求Key
         string GetKeyFromSTKHISDATA(string market, string altSymbol, PeriodType pt)
         {
             return string.Format("{0}:{1}:{2}", market, altSymbol, (int)pt);
         }
 
+        // 请求历史数据
         public void SendHistoricalDataRequestBar(HistoricalDataRequest request, string market)
         {
             Instrument inst = request.Instrument as Instrument;
@@ -296,11 +380,11 @@ namespace QuantBox.OQ.Esunny
 
             string key = GetKeyFromSTKHISDATA(market, altSymbol, pt);
 
+            // 先发请求再存下来
             int nRet = QuotApi.QT_RequestHistory(m_pQuotApi, market, altSymbol, pt);
             if (0 == nRet)
             {
-                historicalDataIds[request.RequestId] = request;
-                historicalDataRecords[key] = request.RequestId;
+                SaveRequest(request, key, market, altSymbol, DateTime.MaxValue);
             }
             else
             {
@@ -311,48 +395,43 @@ namespace QuantBox.OQ.Esunny
         private void OnRspHistoryQuot(IntPtr pQuotApi, IntPtr pBuffer, ref STKHISDATA pHisData)
         {
             string key = GetKeyFromSTKHISDATA(ref pHisData);
-            string requestId;
-            if (historicalDataRecords.TryGetValue(key, out requestId))
+            DataRecord dr;
+            if (historicalDataRecords_key.TryGetValue(key, out dr))
             {
-                HistoricalDataRequest request;
-                if (historicalDataIds.TryGetValue(requestId, out request))
-                {
-                    IntPtr ptrHead = (IntPtr)(pBuffer + Marshal.SizeOf(typeof(STKHISDATA)));
-                    for (int i = 0; i < pHisData.nCount; ++i)
-                    {
-                        IntPtr ptr = (IntPtr)(ptrHead + Marshal.SizeOf(typeof(HISTORYDATA)) * i);
-                        HISTORYDATA hd = (HISTORYDATA)Marshal.PtrToStructure(ptr, typeof(HISTORYDATA));
+                HistoricalDataRequest request = dr.request;
 
-                        if (request.DataType == HistoricalDataType.Daily)
+                IntPtr ptrHead = (IntPtr)(pBuffer + Marshal.SizeOf(typeof(STKHISDATA)));
+                for (int i = 0; i < pHisData.nCount; ++i)
+                {
+                    IntPtr ptr = (IntPtr)(ptrHead + Marshal.SizeOf(typeof(HISTORYDATA)) * i);
+                    HISTORYDATA hd = (HISTORYDATA)Marshal.PtrToStructure(ptr, typeof(HISTORYDATA));
+
+                    if (request.DataType == HistoricalDataType.Daily)
+                    {
+                        DateTime datetime = Convert.ToDateTime(hd.time.Substring(0, 10));
+
+                        if (datetime >= request.BeginDate && datetime < request.EndDate)
                         {
-                            DateTime datetime = Convert.ToDateTime(hd.time.Substring(0, 10));
-                            
-                            if (datetime >= request.BeginDate && datetime < request.EndDate)
-                            {
-                                Daily daily = new Daily(datetime,
-                                                                hd.fOpen, hd.fHigh, hd.fLow, hd.fClose, (long)hd.fVolume, (long)hd.fAmount);
-                                NewHistoricalBar(this,
-                                    new HistoricalBarEventArgs(daily, request.RequestId, request.Instrument, this, -1));
-                            }
-                        }
-                        else
-                        {
-                            DateTime datetime = Convert.ToDateTime(hd.time);
-                            if (datetime >= request.BeginDate && datetime < request.EndDate)
-                            {
-                                Bar bar = new Bar(BarType.Time, request.BarSize, datetime.AddSeconds(-request.BarSize),
-                                    datetime,
-                                    hd.fOpen, hd.fHigh, hd.fLow, hd.fClose, (long)hd.fVolume, (long)hd.fAmount);
-                                NewHistoricalBar(this,
-                                    new HistoricalBarEventArgs(bar, request.RequestId, request.Instrument, this, -1));
-                            }
+                            Daily daily = new Daily(datetime,
+                                                            hd.fOpen, hd.fHigh, hd.fLow, hd.fClose, (long)hd.fVolume, (long)hd.fAmount);
+                            NewHistoricalBar(this,
+                                new HistoricalBarEventArgs(daily, request.RequestId, request.Instrument, this, -1));
                         }
                     }
-
-                    historicalDataIds.Remove(request.RequestId);
-                    EmitHistoricalDataCompleted(request);
+                    else
+                    {
+                        DateTime datetime = Convert.ToDateTime(hd.time);
+                        if (datetime >= request.BeginDate && datetime < request.EndDate)
+                        {
+                            Bar bar = new Bar(BarType.Time, request.BarSize, datetime.AddSeconds(-request.BarSize), datetime,
+                                hd.fOpen, hd.fHigh, hd.fLow, hd.fClose, (long)hd.fVolume, (long)hd.fAmount);
+                            NewHistoricalBar(this,
+                                new HistoricalBarEventArgs(bar, request.RequestId, request.Instrument, this, -1));
+                        }
+                    }
                 }
-                historicalDataRecords.Remove(key);
+
+                RemoveRequest(key);
             }
         }
         #endregion
